40bd004ae228feac5cd1144e021e61ad68bfb6e7
Tomasz Sosnowski <tsosnowski666@gmail.com>
2020-07-21 10:54:52 +0200

    added mapping of letters and numbers to signs

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import converter.MorseConverter;
+
+public class Main {
+    public static void main(String[] args) {
+        MorseConverter converter = new MorseConverter();
+    }
+}
---
+++ src/main/java/converter/MorseConverter.java
@@ -0,0 +1,62 @@
+package converter;
+
+public class MorseConverter {
+
+    private String[] letters;
+    private String[] numbers;
+
+    public MorseConverter(){
+        this.letters = new String[26];
+        this.letters = fillLetterArrayWithSigns(letters);
+
+        this.numbers = new String[10];
+        this.numbers = fillNumberArrayWithSigns(numbers);
+
+    }
+
+    private String[] fillLetterArrayWithSigns(String[] letters){
+        letters[0] = "._"; //A
+        letters[1] = "_..."; //B
+        letters[2] = "_._."; //C
+        letters[3] = "_.."; //D
+        letters[4] = "."; //E
+        letters[5] = ".._."; //F
+        letters[6] = "__."; //G
+        letters[7] = "...."; //H
+        letters[8] = ".."; //I
+        letters[9] = ".___"; //J
+        letters[10] = "_._"; //K
+        letters[11] = "._.."; //L
+        letters[12] = "__"; //M
+        letters[13] = "_."; //N
+        letters[14] = "___"; //O
+        letters[15] = ".__."; //P
+        letters[16] = "__._"; //Q
+        letters[17] = "._."; //R
+        letters[18] = "..."; //S
+        letters[19] = "_"; //T
+        letters[20] = ".._"; //U
+        letters[21] = "..._"; //V
+        letters[22] = ".__"; //W
+        letters[23] = "_.._"; //X
+        letters[24] = "_.__"; //Y
+        letters[25] = "__.."; //Z
+
+        return letters;
+    }
+
+    private String[] fillNumberArrayWithSigns(String[] numbers) {
+        numbers[0] = "_____"; //0
+        numbers[1] = ".____"; //1
+        numbers[2] = "..___"; //2
+        numbers[3] = "...__"; //3
+        numbers[4] = "...._"; //4
+        numbers[5] = "....."; //5
+        numbers[6] = "_...."; //6
+        numbers[7] = "__..."; //7
+        numbers[8] = "___.."; //8
+        numbers[9] = "____."; //9
+
+        return numbers;
+    }
+}
